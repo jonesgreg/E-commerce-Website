6d788fb9e31f941543b205208958d4713aac1d8c
Megha <mu721466@sju.edu>
2019-04-28 18:44:18 -0400

    updating db-register.php

--- Avalon E-commerce Website/db-register.php
+++ Avalon E-commerce Website/db-register.php
@@ -2,20 +2,22 @@
 	if(isset($_POST['Submit'])) {
     require_once('PHP/connection.php');
 
-      $sql = "INSERT INTO user(user_id, user_name, email, phone_number, gender, user_password, street, city, zipcode, state)
-      VALUES (DEFAULT,?,?,?,?,?,?,?,?,?)";
+      // $sql = "INSERT INTO user(user_id, user_name, email, phone_number, gender, user_password, street, city, zipcode, state)
+      // VALUES (DEFAULT,?,?,?,?,?,?,?,?,?)";
+
+			$sql = "call checkEmailExists(?,?,?,?,?,?,?,?,?)";
       $stmt = mysqli_prepare($dbc, $sql);
       mysqli_stmt_bind_param($stmt, "sssssssss",$_POST['user_name'], $_POST['email'], $_POST['phone_number'], $_POST['gender'], $_POST['user_password'],
        $_POST['street'], $_POST['city'], $_POST['zipcode'], $_POST['state']);
       mysqli_stmt_execute($stmt);
       $affected_rows = mysqli_stmt_affected_rows($stmt);
       if($affected_rows == 1){
-        header('Location: login.php?status=true');
+        header('Location: login.php?login=true');
         mysqli_stmt_close($stmt);
         mysqli_close($dbc);
       } else {
-        echo 'Error Occurred<br />';
-        echo mysqli_error();
+				        header('Location: register.php?status=true');
+							        echo mysqli_error($dbc);
         mysqli_stmt_close($stmt);
         mysqli_close($dbc);
       }
